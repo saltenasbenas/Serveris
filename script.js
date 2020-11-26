6dc423971f5f86b6ce933f8f3eacc66206db70c9
sanebas2 <saltenas.benas@gmail.com>
2020-11-26 09:47:15 +0200

    Nerodo Kolekciju ir Labradoru

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let currentDog;
+let currentLabrador;
+let currentCollection;
 
 const UI = {
     newDogButton: document.querySelector(".newDogButton"),
+    newCollectionButton: document.querySelector(".newCollectionButton"),
+    newLabradorButton: document.querySelector(".newLabradorButton"),
     dogImage: document.querySelector(".dogImage"),
- 
+    dogCollection: document.querySelector(".dogCollection"),
+    dogLabrador: document.querySelector(".dogLabrador"),
 };
 
 UI.newDogButton.addEventListener("click", () => {
     fetchDog();
 });
+UI.newCollectionButton.addEventListener("click", () => {
+    fetchCollection();
+});
+UI.newLabradorButton.addEventListener("click", () => {
+    fetchLabrador();
+});
+
 
 
 function fetchDog () {fetch("https://dog.ceo/api/breeds/image/random").then((response) => {
@@ -25,7 +37,40 @@ function fetchDog () {fetch("https://dog.ceo/api/breeds/image/random").then((res
 });
 }
 
+function fetchLabrador () {fetch("https://dog.ceo/api/breed/Labrador/images/random").then((response) => {
+    console.log("Atsakymas gautas is serverio", response);
+    
+    response.json().then((data) => {
+    //    console.log("Suo gautas ir paverstas i JSON", data);
+       currentLabrador = data;
+       console.log(currentLabrador);
+       renderLabrador();
+       
+
+    });
+});
+}
+
+function fetchCollection () {fetch("https://dog.ceo/api/breed/hound/images/random/3").then((response) => {
+    console.log("Atsakymas gautas is serverio", response);
+    
+    response.json().then((data) => {
+    //    console.log("Suo gautas ir paverstas i JSON", data);
+       currentCollection = data;
+       console.log(currentCollection);
+       renderCollection();
+       
+
+    });
+});
+}
 
 function renderDog () {
-    UI.dogImage.src= currentDog;
+    UI.dogImage.src= currentDog.message;
+};
+function renderCollection () {
+    UI.dogCollection.src= currentCollection.message;
+};
+function renderLabrador () {
+    UI.dogLabrador.src= currentLabrador.message;
 };
